924ded6041105cc4144bd61220935ab780b338f5
brianyen <brianyen1000@gmail.com>
2024-03-07 10:13:08 -0800

    add lab6 implementations

--- TestListExamples.java
+++ TestListExamples.java
@@ -2,6 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
 
 class IsMoon implements StringChecker {
   public boolean checkString(String s) {
@@ -9,7 +10,20 @@ class IsMoon implements StringChecker {
   }
 }
 
+class StringMatcher implements StringChecker {
+  String match;
+
+  public StringMatcher(String match) {
+      this.match = match;
+  }
+
+  public boolean checkString(String s) {
+      return s.contains(match);
+  }
+}
+
 public class TestListExamples {
+  @SuppressWarnings("unchecked")
   @Test(timeout = 500)
   public void testMergeRightEnd() {
     List<String> left = Arrays.asList("a", "b", "c");
@@ -17,5 +31,58 @@ public class TestListExamples {
     List<String> merged = ListExamples.merge(left, right);
     List<String> expected = Arrays.asList("a", "a", "b", "c", "d");
     assertEquals(expected, merged);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test(timeout = 500)
+  public void testFilterMoon() {
+    StringChecker moonChecker = new IsMoon();
+    List<String> strInput = new ArrayList();
+    List<String> strInput2 = new ArrayList();
+    strInput.add("moon");
+    strInput.add("MOON");
+    strInput.add("earth");
+    strInput.add("MoOn");
+    strInput.add("jupiter moon");
+
+    strInput2.add("moon");
+    strInput2.add("MOON");
+    strInput2.add("MoOn");
+
+    List<String> output = ListExamples.filter(strInput, moonChecker);
+    assertEquals(strInput2, output);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test(timeout = 500)
+  public void testFilter() {
+      StringChecker checker1 = new StringMatcher("l");
+      StringChecker checker = new StringMatcher("he");
+      List<String> strInput = new ArrayList<>();
+      List<String> strInput2 = new ArrayList<>();
+      List<String> strInput3 = new ArrayList<>();
+      strInput.add("hello");
+      strInput.add("world");
+      strInput.add("helloo");
+      strInput2.add("hello");
+      strInput2.add("helloo");
+      strInput3.add("hello");
+      strInput3.add("world");
+      strInput3.add("helloo");
+      List<String> output1 = ListExamples.filter(strInput, checker1);
+      List<String> output = ListExamples.filter(strInput, checker);
+      assertEquals(output1, strInput3);
+      assertEquals(output, strInput2);
+  }
+
+  @SuppressWarnings("unchecked")
+  @Test(timeout = 500)
+  public void testMerge() {
+      List<String> strInput1 = new ArrayList<>();
+      List<String> strInput2 = new ArrayList<>();
+
+      strInput2.add("string!");
+
+      assertEquals(strInput2, ListExamples.merge(strInput1, strInput2));
   }
 }
--- grade.sh
+++ grade.sh
@@ -5,9 +5,43 @@ rm -rf grading-area
 
 mkdir grading-area
 
-git clone $1 student-submission
+git clone $1 student-submission 2> grading-area/clone-output.txt
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo 'Necessary files found'
+
+    cp student-submission/ListExamples.java TestListExamples.java grading-area
+    cp -r lib grading-area
+else
+    echo "Missing ListExamples.java file. Have you checked its name and path?"
+    exit 1
+fi
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [[ $? -ne 0 ]]
+then
+    echo "The code failed to compile! See error above."
+    exit 1
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > test-output.txt
+
+OUTPUT=$(cat test-output.txt | tail -n 2 | head -n 1)
+
+if [[ $(echo $OUTPUT | cut -d " " -f 1) == "OK" ]]
+then
+    echo "All $(echo $OUTPUT | cut -d "(" -f 2 | cut -d " " -f 1) Tests Passed! Grade: 100%"
+else
+    TOTAL=$(echo $OUTPUT | cut -d " " -f 3 | cut -d "," -f 1)
+    FAILED=$(echo $OUTPUT | cut -d " " -f 5)
+    PASSED=$(($TOTAL - $FAILED))
+    echo "$PASSED/$TOTAL Tests Passed. Grade: $(echo $PASSED/$TOTAL*100 | bc -l | head -c 5)%"
+fi
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
